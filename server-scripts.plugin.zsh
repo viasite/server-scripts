aa1e69882c720a078f3165e20316e8632e6fc21d
Stanislav Popov <popstas@gmail.com>
2017-04-23 01:45:53 +0500

    feat: completions for dbundump, mysql_backup, zipsite

--- server-scripts.plugin.zsh
+++ server-scripts.plugin.zsh
@@ -14,9 +14,27 @@ _ss_dbdump() {
     esac
 }
 
-# add-users
-# dbundump
+_ss_file_db() {
+    _arguments -C \
+    "1:first_arg:_files" \
+    "2:first_arg:->db"
+
+    case "$state" in
+        db)
+            dbs=$(__dbs_list)
+            compadd "$@" $(echo $dbs)
+            ;;
+    esac
+}
+
+_ss_mysql_backup() {
+    compadd "$@" backup send clean
+}
+
 # goto-site
 
 compdef _ss_dbdump dbdump
+compdef _ss_file_db dbundump
+compdef _ss_mysql_backup mysql_backup
 compdef _users user-setup
+compdef _ss_file_db zipsite
